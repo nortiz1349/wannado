933bc07e05127e1e96148505fc74b478173f88dd
Nortiz <nortiz1349@gmail.com>
2022-06-20 15:18:30 +0900

    Initial commit

---
+++ src/main/java/com/project/wannado/WannadoApplication.java
@@ -0,0 +1,13 @@
+package com.project.wannado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/wannado/WannadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wannado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WannadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
